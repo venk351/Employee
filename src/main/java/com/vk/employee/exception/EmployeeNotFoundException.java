e0e1bddeab1873e5539655b36e9a26c3bddc63c8
Venkatesh Subramani <52658916+venk351@users.noreply.github.com>
2023-11-02 13:03:57 +0530

    Create EmployeeNotFoundException.java

---
+++ src/main/java/com/vk/employee/exception/EmployeeNotFoundException.java
@@ -0,0 +1,20 @@
+package com.vk.employee.Exception;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+	public EmployeeNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public EmployeeNotFoundException(String message) {
+		super(message);
+	}
+
+	public EmployeeNotFoundException(Throwable cause) {
+		super(cause);
+	}
+	
+	
+	
+
+}
